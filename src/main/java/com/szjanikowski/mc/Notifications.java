44b6f1093284200eaa62524bc082f41271f1155b
Szymon Janikowski <sz.janikowski@gmail.com>
2020-05-11 18:33:49 +0200

    Logic

--- src/main/java/com/szjanikowski/mc/Notifications.java
+++ src/main/java/com/szjanikowski/mc/Notifications.java
@@ -2,27 +2,14 @@ package com.szjanikowski.mc;
 
 import io.micronaut.context.annotation.Context;
 
-import javax.annotation.PostConstruct;
-
 @Context
 public class Notifications {
 
-	private final ServerMonitoring serverMonitoring;
-	private final PubSubMsgSending pubSubMsgSending;
-
 	public Notifications(ServerMonitoring serverMonitoring, PubSubMsgSending pubSubMsgSending) {
-		this.serverMonitoring = serverMonitoring;
-		this.pubSubMsgSending = pubSubMsgSending;
-	}
-
-	@PostConstruct
-	public void init() {
-		serverMonitoring.getNumberOfPlayersOnServer()
-				.filter(numberOfPlayers -> numberOfPlayers == 0)
-				.window(5) //TODO config !!
-				.subscribe(window -> pubSubMsgSending.publishNoPlayersForSeconds(5));
+		serverMonitoring.getZeroPlayersPeriod()
+				.filter(time -> time > 5000)
+				.firstOrError()
+				.subscribe(time -> pubSubMsgSending.publishNoPlayersForSeconds(5));
 
 	}
-
-
 }
--- src/main/java/com/szjanikowski/mc/ServerMonitoring.java
+++ src/main/java/com/szjanikowski/mc/ServerMonitoring.java
@@ -1,7 +1,6 @@
 package com.szjanikowski.mc;
 
 import io.micronaut.scheduling.annotation.Scheduled;
-import io.netty.util.internal.ThreadLocalRandom;
 import io.reactivex.Observable;
 import io.reactivex.subjects.BehaviorSubject;
 
@@ -12,14 +11,32 @@ public class ServerMonitoring {
 
 	private final BehaviorSubject<Integer> numberOfPlayersOnServer = BehaviorSubject.create();
 
-
-	public Observable<Integer> getNumberOfPlayersOnServer() {
-		return numberOfPlayersOnServer;
-	}
+	private final Observable<Long> zeroPlayersPeriod;
 
 	@Scheduled(fixedRate = "1s")
 	public void checkForNumberOfPlayers() {
-		numberOfPlayersOnServer.onNext(ThreadLocalRandom.current().nextInt(0, 6));
+//		numberOfPlayersOnServer.onNext(ThreadLocalRandom.current().nextInt(0, 6));
+		numberOfPlayersOnServer.onNext(0);
+	}
+
+	public ServerMonitoring() {
+		zeroPlayersPeriod = numberOfPlayersOnServer
+				.timestamp()
+				.scan((startOfZeroPlayersPeriod, numberOfPlayersWithTime) ->
+						numberOfPlayersWithTime.value() > 0 ?
+								numberOfPlayersWithTime : startOfZeroPlayersPeriod
+				)
+				.timestamp()
+				.map(startOfZeroPlayersWithCurrentTime -> {
+					long currentTime = startOfZeroPlayersWithCurrentTime.time();
+					long timeOfZeroPlayersPeriodStart = startOfZeroPlayersWithCurrentTime.value().time();
+					return currentTime - timeOfZeroPlayersPeriodStart;
+				});
+
+	}
+
+	public Observable<Long> getZeroPlayersPeriod() {
+		return zeroPlayersPeriod;
 	}
 
 }
