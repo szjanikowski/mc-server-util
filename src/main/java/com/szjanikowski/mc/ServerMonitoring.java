887c5541abd31435d085525558f56fc0a46541e3
Szymon Janikowski <sz.janikowski@gmail.com>
2020-05-15 15:51:10 +0200

    Logging 2

--- src/main/java/com/szjanikowski/mc/ServerMonitoring.java
+++ src/main/java/com/szjanikowski/mc/ServerMonitoring.java
@@ -1,13 +1,18 @@
 package com.szjanikowski.mc;
 
+import com.szjanikowski.mc.actions.gcpsdk.GoogleCloudSdkActions;
 import io.micronaut.scheduling.annotation.Scheduled;
 import io.reactivex.Observable;
 import io.reactivex.subjects.BehaviorSubject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class ServerMonitoring {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ServerMonitoring.class);
 
 	private final BehaviorSubject<Integer> numberOfPlayersOnServer = BehaviorSubject.create();
 
@@ -20,6 +25,7 @@ public class ServerMonitoring {
 	@Scheduled(fixedRate = "${mcutil.checking.frequency:20s}")
 	public void checkForNumberOfPlayers() {
 		int numberOfPlayers = minecraftServerApi.getCurrentNumberOfPlayers();
+		LOG.info("Number of players: " + numberOfPlayers);
 		numberOfPlayersOnServer.onNext(numberOfPlayers);
 	}
 
